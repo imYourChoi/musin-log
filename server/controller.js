d8f49bd3b4c16098fb01c4ffca27b669b0b13018
imYourChoi <jeehun@kaist.ac.kr>
2023-07-30 21:47:41 +0900

    Add: sort options

--- server/controller.js
+++ server/controller.js
@@ -1,23 +1,48 @@
 const { productModel } = require("./model");
 
+const getSortOption = (sort) => {
+  switch (sort) {
+    case "price_asc":
+      return { original_price: 1 };
+    case "price_desc":
+      return { original_price: -1 };
+    case "name_asc":
+      return { name: 1 };
+    case "name_desc":
+      return { name: -1 };
+    case "brand_asc":
+      return { brand: 1 };
+    case "brand_desc":
+      return { brand: -1 };
+    default:
+      return { _id: 1 };
+  }
+};
+
 const allProductHandler = async (req, res) => {
   try {
-    const products = await productModel.find({}, { price_history: 0, _id: 0 });
+    const { sort } = req.query;
+    const products = await productModel
+      .find({}, { price_history: 0, _id: 0 })
+      .sort(getSortOption(sort));
     res.status(200).json({ products });
   } catch (error) {
     res.status(500).json({
-      error: "Products/allProducts : Internal Server Error",
+      error: "Internal Server Error : allProducts",
     });
   }
 };
 
 const allProductDetailHandler = async (req, res) => {
   try {
-    const products = await productModel.find({}, { _id: 0 });
+    const { sort } = req.query;
+    const products = await productModel
+      .find({}, { _id: 0 })
+      .sort(getSortOption(sort));
     res.status(200).json({ products });
   } catch (error) {
     res.status(500).json({
-      error: "Products/allProducts : Internal Server Error",
+      error: "Internal Server Error : allProductsDetail",
     });
   }
 };
@@ -32,27 +57,29 @@ const oneProductHandler = async (req, res) => {
     res.status(200).json({ product });
   } catch (error) {
     res.status(500).json({
-      error: "Products/oneProduct : Internal Server Error",
+      error: "Internal Server Error : oneProduct",
     });
   }
 };
 
 const searchProductsHandler = async (req, res) => {
   try {
-    const { keyword } = req.query;
-    const products = await productModel.find(
-      {
-        $or: [
-          { name: { $regex: keyword, $options: "i" } },
-          { brand: { $regex: keyword, $options: "i" } },
-        ],
-      },
-      { price_history: 0, _id: 0 }
-    );
+    const { keyword, sort } = req.query;
+    const products = await productModel
+      .find(
+        {
+          $or: [
+            { name: { $regex: keyword, $options: "i" } },
+            { brand: { $regex: keyword, $options: "i" } },
+          ],
+        },
+        { price_history: 0, _id: 0 }
+      )
+      .sort(getSortOption(sort));
     res.status(200).json({ products });
   } catch (error) {
     res.status(500).json({
-      error: "Products/searchProducts : Internal Server Error",
+      error: "Internal Server Error : searchProducts",
     });
   }
 };
