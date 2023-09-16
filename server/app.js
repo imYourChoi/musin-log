bd4497f932842bd385732aeb6510bd95ca8c2977
imYourChoi <jeehun@kaist.ac.kr>
2023-09-16 22:27:42 +0900

    Refactor: add src folder to server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { FRONT_URL: frontUrl, PORT: port } = require("./env");
+const { FRONT_URL: frontUrl, PORT: port } = require("./src/env");
 
 const app = express();
 
-require("./db");
+require("./src/db");
 
 const corsOptions = { origin: [frontUrl], credentials: true };
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.use("/", require("./router"));
+app.use("/", require("./src/router"));
 
 app.listen(port, () => {
   console.log(`Express 서버가 ${port}번 포트에서 실행됩니다.`);
